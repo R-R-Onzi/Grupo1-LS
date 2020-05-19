f8d94f6fd650e0ff7b14cb1b2c83a509290c0cf1
HackIssue5560 <57115645+HackIssue5560@users.noreply.github.com>
2020-05-18 22:16:15 -0300

    Add files via upload

--- telas/imagens.py
+++ telas/imagens.py
@@ -2,30 +2,78 @@
 from tkinter import *
 from PIL import ImageTk , Image
 
+global tupla
+
 root = Tk()
+root.geometry("550x500")
 root.title('Pleno Sono ')
 
 logo = ImageTk.PhotoImage(Image.open('cropped-plenosono_logo-1.png'))
 lg = Label(image = logo)
 
-espaco = Label(root, text = '')
 
 def cad():
-    return
+
+    def confi():
+        tupla = [nome.get().upper(), cnpj.get().upper(), contato.get().upper(), nf.get().upper(), pai.get().upper(), pecas.get().upper()] 
+        #tratamento_registro_cadastro(tupla)
+
+        
+    cadastro = Toplevel(root)
+    cadastro.geometry("500x500")
+    cadastro.title("Cadastro")
+    cadastro.title("Alterar Cadastro")
+
+    mens1 = Label(cadastro, text = "Nome Real", font = 25, padx = 50, pady = 25)
+    mens2 = Label(cadastro, text = "CNPJ", font = 25, padx = 50, pady = 25)
+    mens3 = Label(cadastro, text = "Contato", font = 25, padx = 50, pady = 25)
+    mens4 = Label(cadastro, text = "Niível de Formação", font = 50, padx = 25, pady = 25)
+    mens5 = Label(cadastro, text = "Nome do Pai", font = 25, padx = 50, pady = 25)
+    mens6 = Label(cadastro, text = "Peças vendidas", font = 25, padx = 50, pady = 25)
+
+    mens1.grid(row = 0, column = 0)
+    mens2.grid(row = 1, column = 0)
+    mens3.grid(row = 2, column = 0)
+    mens4.grid(row = 3, column = 0)
+    mens5.grid(row = 4, column = 0)
+    mens6.grid(row = 5, column = 0)
+
+    nome = Entry(cadastro, width = 30, font = 25)
+    cnpj = Entry(cadastro, width = 30, font = 25)
+    contato = Entry(cadastro, width = 30, font = 25)
+    nf = Entry(cadastro, width = 30, font = 25)
+    pai = Entry(cadastro, width = 30, font = 25)
+    pecas = Entry(cadastro, width = 30, font = 25)
+
+    nome.grid(row = 0, column = 1)
+    cnpj.grid(row = 1, column = 1)
+    contato.grid(row = 2, column = 1)
+    nf.grid(row = 3, column = 1)
+    pai.grid(row = 4, column = 1)
+    pecas.grid(row = 5, column = 1)
+
+    confirmar = Button(cadastro, text = "Confirmar", font = 20, command = confi)
+
+    confirmar.grid(row = 6, column = 0)
 
 def alt():
-    return
-
+    altCadastro = Toplevel(root)
+    altCadastro.geometry("500x500")
+    
 def gerar():
-    return
+    gera = Toplevel(root)
+    gera.geometry("500x500")
+    gera.title("Gerar Relatório")
 
 def vis():
-    return
+    visualizar = Toplevel(root)
+    visualizar.geometry("500x500")
+    visualizar.title("Visualizar Distribuidor")
 
-Cadas_Dist = Button(root, text = "Cadastrar Distribuidor", command = cad)
-Alt_Dist = Button(root, text = "Alterar Distribuidor", command = alt)
-Ger_Rel = Button(root, text = "Gerar Relatório", command = gerar)
-Vis_Dist = Button(root, text = "Visualizar Distribuidor", command = vis)
+Cadas_Dist = Button(root, text = "Cadastrar Distribuidor", font = 20, command = cad)
+Alt_Dist = Button(root, text = "Alterar Distribuidor", font = 20, command = alt)
+Ger_Rel = Button(root, text = "Gerar Relatório", font = 20, command = gerar)
+Vis_Dist = Button(root, text = "Visualizar Distribuidor", font = 20, command = vis)
 
 Cadas_Dist.grid(row = 0, column = 0, padx = 50, pady = 50)
 Alt_Dist.grid(row = 0, column = 1, padx = 50, pady = 50)
