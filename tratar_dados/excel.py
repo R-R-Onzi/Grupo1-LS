3e133861dbe5074f695bba943a08560ab586f03d
César Carpeggiani <cesarcarpeggiani@gmail.com>
2020-05-25 19:45:46 -0300

    classe bonita pra pegar o total da compra, data e num do pedido da planilha

--- tratar_dados/excel.py
+++ tratar_dados/excel.py
@@ -1,12 +1,28 @@
 import pandas as pd
 
 
-df = pd.read_excel('documentos\pedido.xlsx')
+class Excellent():
+
+    _total = 0.0
+    _data = ''
+    _numpedido = 0
+
+    def __init__(self, caminho):
+        planilha = pd.read_excel(caminho)
+        linha_pagar = planilha.loc[planilha['Unnamed: 7'].isin(['A PAGAR', 'a pagar'])]
+        linha_num_data = planilha.loc[planilha['Unnamed: 7'].isin(['DATA'])]
+        self._numpedido  = linha_num_data.values[0][5]
+        self._data       = linha_num_data.values[0][8]
+        self._total      = linha_pagar.values[0][-1]  # -1 traz o ultimo elemento da lista       
+
+
 
 # print(df.columns)
 
-swag = df.loc[df['Unnamed: 7'].isin(['A PAGAR', 'a pagar'])]
 
-totalpagar = swag.values[0][-1]  # -1 traz o ultimo elemento da lista
+pedido = Excellent('pedido.xlsx')
 
-print(totalpagar)
+print(pedido._numpedido)
+print(pedido._total)
+print(pedido._data)
+
